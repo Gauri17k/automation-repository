3e19086bfc5d557925a76629aca3ed5cff78ba89
gauri <gauri17kulkarni@gmail.com>
2019-08-01 21:24:47 +0530

    commiting class

---
+++ BasicCoreJava/src/vaiablesAndDataTypes/DataTypes.java
@@ -0,0 +1,36 @@
+package vaiablesAndDataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		//integer
+		int age=30;
+		System.out.println(age);
+		
+		//characher
+		
+		char gender ='F';
+		
+		//Decimal values- float or double
+		
+		double marks=7.5;
+		float s=5.1;
+		
+	   //Boolean value
+		boolean status= true;
+		
+		// strings
+		
+		String name='Gauri';
+		
+		// for integer values
+		byte a=10;
+		short b=20;
+		int c= 30;
+		long d=40;
+		
+		
+	}
+
+}
